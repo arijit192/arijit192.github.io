9c87e4869b0aea3e5219c7e58b9e3867d794c31f
Arijit Roy <tukaiari@gmail.com>
2019-01-28 20:44:51 +0530

    Initial Upload

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,8 @@
+function mobile(){
+  if (screen.width <= 699) {
+    document.location = "/m/index.html";
+  }
+};
+function test(){
+  document.getElementById("test").innerHTML= "You have pressed";
+};
